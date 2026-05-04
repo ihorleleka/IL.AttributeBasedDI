e1c38118e6a873ac901ea68a421a5e939f61028a
Ihor Leleka <ile@novicell.co.uk>
2026-05-04 12:43:35 +0100

    chore: updated vv version

--- Code/IL.AttributeBasedDI.Visualizer/Extensions/DiRegistrationSummaryExtensions.cs
+++ Code/IL.AttributeBasedDI.Visualizer/Extensions/DiRegistrationSummaryExtensions.cs
@@ -9,7 +9,7 @@ public static class DiRegistrationSummaryExtensions
 {
     public static void AddVisualizer(this DiRegistrationSummary summary)
     {
-        summary.Services.AddVirtualViewsCapabilities();
+        summary.Services.AddVirtualViewsCapabilities("*");
         summary.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
         summary.Services.AddKeyedSingleton(Constants.ServiceGraphKey, summary.ServiceGraph);
         summary.Services.AddKeyedSingleton<IViewRenderService, ViewRenderService>(Constants.ViewRendererServiceKey);
